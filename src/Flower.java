beb238db4ed1d855b0433586be7c52ac7c1be4c7
JelizavetaKuznetsova <jelizaveta.kuznetsova@gmail.com>
2020-03-15 22:00:46 +0200

    Basis todos and flower class.

---
+++ src/Flower.java
@@ -0,0 +1,8 @@
+// TODO read/write file txt
+// TODO flower class: water, sunlight, love/talk, food
+// TODO time functions
+// TODO relations between functions of watering, putting into the sunlight, talk/love/cuddling, giving the flower nutrition
+
+public class Flower {
+
+}
